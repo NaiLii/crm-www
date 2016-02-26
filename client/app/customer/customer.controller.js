95c35d16da19282b58bd4c686be34bd8df8f2f2f
lilianshuan <vs2@live.cn>
2016-02-26 21:58:57 +0800

    客户的前端简易搜索

--- client/app/customer/customer.controller.js
+++ client/app/customer/customer.controller.js
@@ -5,15 +5,34 @@
 (function () {
 	function CustomerController($scope, $http, $location) {
 		$scope.customers = [];
+		$scope.cloneCustomers = [];
+		$scope.search = {
+			keyword:''
+		};
 		$scope.getCustomers = function () {
 			$http.get('/api/customer/my')
 				.success(function (data) {
 					$scope.customers = data;
+					$scope.cloneCustomers = _.clone(data);
+					console.log(data);
 				});
 		};
 		$scope.getCustomers();
 		$scope.showDetail = function (cid) {
 			$location.url('/customer/detail/' + cid);
+		};
+		$scope.newCustomer = function(){
+			$location.url('/customer/add');
+		};
+		$scope.changeWord = function(){
+			var key = $scope.search.keyword.trim();
+			if(key){
+				$scope.customers = $scope.cloneCustomers.filter(function (x) {
+					return x.name.indexOf(key)>-1;
+				});
+			}else{
+				$scope.customers = _.clone($scope.cloneCustomers);
+			}
 		};
 	}
 
