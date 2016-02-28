0b98fb68e0113152df4864d571dcb057082016a0
lilianshuan <vs2@live.cn>
2016-02-28 22:31:16 +0800

    通过登录获取令牌，并存于cookie中。

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,26 +1,36 @@
 'use strict';
-
 angular.module('crmWwwApp')
-  .controller('LoginCtrl', function($scope, Auth, $state) {
-    $scope.user = {};
-    $scope.errors = {};
+	.controller('LoginCtrl', function ($scope, $http, $state, $cookies) {
+		$scope.user = {};
+		$scope.errors = {};
+		$scope.login = function (form) {
+			$scope.submitted = true;
+			if (form.$valid) {
+				$http.post('/api/auth/login',{username:$scope.user.username,password:$scope.user.password})
+					.success(function(data){
+						console.log(data);
+						if(data && data.status==='200')
+						{
+							//todo 存放时限，路径等。
+							$cookies.put('token', data.token);
+							$state.go('main');
 
-    $scope.login = function(form) {
-      $scope.submitted = true;
-
-      if (form.$valid) {
-        Auth.login({
-          email: $scope.user.email,
-          password: $scope.user.password
-        })
-        .then(function() {
-          // Logged in, redirect to home
-          $state.go('main');
-        })
-        .catch(function(err) {
-          $scope.errors.other = err.message;
-        });
-      }
-    };
-
-  });
+						}
+					})
+					.error(function(e){
+						console.log(e);
+					});
+				//Auth.login({
+				//	email: $scope.user.email,
+				//	password: $scope.user.password
+				//})
+				//	.then(function () {
+				//		// Logged in, redirect to home
+				//		$state.go('main');
+				//	})
+				//	.catch(function (err) {
+				//		$scope.errors.other = err.message;
+				//	});
+			}
+		};
+	});
