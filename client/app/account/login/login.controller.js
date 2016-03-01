6d9cece4b932d61f6cc62e3d3c1b8cc9677d7adf
lilianshuan <vs2@live.cn>
2016-03-01 22:19:58 +0800

    我的客户显示首字母。未实现排序。

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -6,31 +6,22 @@ angular.module('crmWwwApp')
 		$scope.login = function (form) {
 			$scope.submitted = true;
 			if (form.$valid) {
-				$http.post('/api/auth/login',{username:$scope.user.username,password:$scope.user.password})
-					.success(function(data){
+				var doc = {
+					username: $scope.user.username,
+					password: $scope.user.password
+				};
+				$http.post('/api/auth/login', doc)
+					.success(function (data) {
 						console.log(data);
-						if(data && data.status==='200')
-						{
+						if (data && data.status === '200') {
 							//todo 存放时限，路径等。
 							$cookies.put('token', data.token);
 							$state.go('main');
-
 						}
 					})
-					.error(function(e){
+					.error(function (e) {
 						console.log(e);
 					});
-				//Auth.login({
-				//	email: $scope.user.email,
-				//	password: $scope.user.password
-				//})
-				//	.then(function () {
-				//		// Logged in, redirect to home
-				//		$state.go('main');
-				//	})
-				//	.catch(function (err) {
-				//		$scope.errors.other = err.message;
-				//	});
 			}
 		};
 	});
--- client/app/customer/customer.controller.js
+++ client/app/customer/customer.controller.js
@@ -7,30 +7,42 @@
 		$scope.customers = [];
 		$scope.cloneCustomers = [];
 		$scope.search = {
-			keyword:''
+			keyword: ''
 		};
+		$scope.customerList = {};
 		$scope.getCustomers = function () {
 			$http.get('/api/customer/my')
 				.success(function (data) {
 					$scope.customers = data;
 					$scope.cloneCustomers = _.clone(data);
 					console.log(data);
+					$scope.customers.forEach(function (c) {
+						var letter = c.letter.substring(0, 1).toUpperCase();
+						c.group = letter;
+						if ($scope.customerList[letter]) {
+							$scope.customerList[letter].push(c);
+						} else {
+							$scope.customerList[letter] = [];
+							$scope.customerList[letter].push(c);
+						}
+					});
+					$scope.customerList.sort(function(a,b){return a.letter > b.letter;});
 				});
 		};
 		$scope.getCustomers();
 		$scope.showDetail = function (cid) {
 			$location.url('/customer/detail/' + cid);
 		};
-		$scope.newCustomer = function(){
+		$scope.newCustomer = function () {
 			$location.url('/customer/add');
 		};
-		$scope.changeWord = function(){
+		$scope.changeWord = function () {
 			var key = $scope.search.keyword.trim();
-			if(key){
+			if (key) {
 				$scope.customers = $scope.cloneCustomers.filter(function (x) {
-					return x.name.indexOf(key)>-1;
+					return x.name.indexOf(key) > -1;
 				});
-			}else{
+			} else {
 				$scope.customers = _.clone($scope.cloneCustomers);
 			}
 		};
